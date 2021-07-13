d76e100cf2d737d12b4ce99e5a103eadb53f4e6d
Nick <kanishevnikita@yandex.kz>
2021-07-14 01:45:33 +0600

    created categories section

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ $(function () {
       '<button class="slick-btn slick-btn__prev"><img src="images/arrow-left.svg" alt="leftArrow"></button>',
     nextArrow:
       '<button class="slick-btn slick-btn__next"><img src="images/arrow-right.svg" alt="nextArrow"></button>',
-  })
+  });
 
   $(".search__tabs-item").on("click", function (e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    $(".search__tabs-item").removeClass("search__tabs-item--active")
-    $(".search__content-item").removeClass("search__content-item--active")
+    $(".search__tabs-item").removeClass("search__tabs-item--active");
+    $(".search__content-item").removeClass("search__content-item--active");
 
-    $(this).addClass("search__tabs-item--active")
-    $($(this).data("tab")).addClass("search__content-item--active")
-  })
-})
+    $(this).addClass("search__tabs-item--active");
+    $($(this).data("tab")).addClass("search__content-item--active");
+  });
+});
