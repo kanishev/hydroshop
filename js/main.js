79647f1ec9c45bbd75c9bd8427fdd512509fcb17
Nick <kanishevnikitaa@gmail.com>
2021-07-13 19:13:12 +0600

    created main slider section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
-});
+$(function () {
+  $(".banner-section__slider").slick({
+    dots: true,
+    prevArrow:
+      '<button class="slick-btn slick-btn__prev"><img src="images/arrow-left.svg" alt="leftArrow"></button>',
+    nextArrow:
+      '<button class="slick-btn slick-btn__next"><img src="images/arrow-right.svg" alt="nextArrow"></button>',
+  })
+})
