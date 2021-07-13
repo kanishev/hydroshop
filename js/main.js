73af04a3eabba96be17aff276640f53c5f2d5ae5
Nick <kanishevnikitaa@gmail.com>
2021-07-14 00:42:45 +0600

    created main tabs section

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ $(function () {
     nextArrow:
       '<button class="slick-btn slick-btn__next"><img src="images/arrow-right.svg" alt="nextArrow"></button>',
   })
+
+  $(".search__tabs-item").on("click", function (e) {
+    e.preventDefault()
+
+    $(".search__tabs-item").removeClass("search__tabs-item--active")
+    $(".search__content-item").removeClass("search__content-item--active")
+
+    $(this).addClass("search__tabs-item--active")
+    $($(this).data("tab")).addClass("search__content-item--active")
+  })
 })
