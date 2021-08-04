b2d2625e710eab5be8e19c56afad89eab0d0938a
Nick <kanishevnikita@yandex.kz>
2021-08-05 00:50:56 +0600

    backend branch cleaned

--- src/components/UI/index.js
+++
@@ -1,6 +0,0 @@
-import AppButton from "./AppButton.vue";
-import AppRange from "./AppRange.vue";
-import AppTab from "./AppTab.vue";
-import AppFilter from "./AppFilter.vue";
-
-export default [AppButton, AppTab, AppFilter, AppRange];
